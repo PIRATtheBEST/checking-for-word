f5aeb6048761a6bd0526029696179c09fc825d74
PIRATtheBEST <alexalex0072200722@gmail.com>
2018-11-20 23:30:23 +0300

    c Distinct

--- Program.cs
+++ Program.cs
@@ -15,18 +15,11 @@ namespace ConsoleApp7
             string[] razb = str.Split(' ', ',');
             bool checking = false;
             int count = 0;
-            List<string> template = new List<string> { };
-            for (int i = 0; i < razb.Length; i++)
+            var razb1 = razb.Distinct();
+            foreach(string s in razb1)
             {
-                template.Add(razb[i]);
+                count++;
             }
-            for (int i = 0; i < (razb.Length-1); i++)
-            {
-                if (!(template[i].Equals(template[i + 1])))
-                {
-                    count++;
-                }
-            };
             Console.WriteLine("колличество уникальных слов - " + count);
 
             Console.ReadLine();
