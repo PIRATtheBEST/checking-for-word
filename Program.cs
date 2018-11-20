d59b59a07829cdb83068d2d3d304062aeed9861d
PIRATtheBEST <alexalex0072200722@gmail.com>
2018-11-20 23:22:09 +0300

    с перебором

--- Program.cs
+++ Program.cs
@@ -13,11 +13,21 @@ namespace ConsoleApp7
             Console.WriteLine("введите строку");
             string str = Console.ReadLine();
             string[] razb = str.Split(' ', ',');
-            var razbnew = razb.Union(razb);
+            bool checking = false;
             int count = 0;
-            foreach (string word in razbnew)
-                count++;
-            Console.WriteLine("колличество уникальных слов - " + (count-1));
+            List<string> template = new List<string> { };
+            for (int i = 0; i < razb.Length; i++)
+            {
+                template.Add(razb[i]);
+            }
+            for (int i = 0; i < (razb.Length-1); i++)
+            {
+                if (!(template[i].Equals(template[i + 1])))
+                {
+                    count++;
+                }
+            };
+            Console.WriteLine("колличество уникальных слов - " + count);
 
             Console.ReadLine();
         }
